6a4cc74f4ac0af2e87425b2e0d86adc1f3ba73cb
Manuqlly <selvaperumal20005@gmail.com>
2025-02-27 13:14:30 +0530

    .

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: "/React-e-plantShopping",
   plugins: [react()],
+  base: '/e-plantShopping/',  // This should match your repo name
 });
